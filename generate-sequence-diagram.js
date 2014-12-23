f5b171a901316f69fd7207ea18426e03ec6bc537
Srdan Srepfler <schrepfler@gmail.com>
2014-12-23 16:25:52 +0100

    moving slowly to a cleaner js solution

--- generate-sequence-diagram.js
+++ generate-sequence-diagram.js
@@ -1,23 +1,62 @@
 #!/usr/bin/env node
 var argv = require('yargs')
     .usage('Generate a sequence diagram from a diagram DSL file and output it as a png image with a given name. \nUsage: $0')
-    .example('$0 -f diagram-file.html -o output-file.png')
+    .example('$0 -f diagram-file.txt -o output-file.png -t [hand|simple]')
     .demand('f')
     .alias('f','input')
     .describe('f', 'Input filename with the diagram DSL')
     .demand('o')
     .alias('o','output')
-    .describe('o', 'Output filename image. Should be .png')
+    .describe('o', 'Output filename image. Should be *.png')
+    .alias('t','type')
+    .describe('t', 'Diagram type.')
+    .default('hand')
     .argv;
 
-var Nightmare = require('nightmare');
+var fs                      = require('fs');
+var Nightmare               = require('nightmare');
+var Handlebars              = require('handlebars');
+var sequenceDiagramTemplate = require('./sequenceDiagram.tpl.js');
 
-new Nightmare()
-  .goto(argv.f)
-  .evaluate(function (page) {
-    return;
-  }, function (res) {
-    console.log(res);
-  })
-  .screenshot(argv.o)
-  .run();
+var sequenceDiagram;
+var sequenceDiagramCaption = "";
+var sequenceDiagramTheme = "hand";
+
+fs.readFile(argv.f, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    sequenceDiagram = data;
+
+    var handlebarsContext = { "sequenceDiagram" : sequenceDiagram,
+        "caption" : sequenceDiagramCaption,
+        "theme": sequenceDiagramTheme };
+
+    var sequenceDiagramOutput = Handlebars.templates.sequenceDiagram(handlebarsContext);
+
+
+    fs.writeFile("tmp.html", sequenceDiagramOutput, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The diagram markup was saved!");
+            makeScreenshot();
+        }
+    });
+
+    var makeScreenshot = function(){
+        new Nightmare()
+            .goto("tmp.html")
+            .evaluate(function (page) {
+                return;
+            }, function (res) {
+                console.log(res);
+            })
+            .screenshot(argv.o)
+            .run();
+    };
+
+
+
+
+});
